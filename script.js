75df585d37df1f4eb297014ede7754652d7c4ee6
hkarambizi <hkarambizi@gmail.com>
2017-03-09 01:43:55 -0500

    added variables to script.js and linked bootstrap and jQuery

---
+++ script.js
@@ -0,0 +1,70 @@
+/*
+Game Variables:
+
+//These are the variables related to each round
+var roundResult;
+var roundWinner;
+var roundLoser;
+var roundTie;
+var roundNumber;
+
+//these are the variables related to the players
+var currentPlayer;
+var computer;
+var userPlayerName;
+var userPlayerThrowVelocity;
+var userPlayer;
+var computerRollPoints;
+var userRollPoints;
+var userDice1PointValue;
+var userDice2PointValue;
+var userDice3PointValue;
+var computerDice1PointValue;
+var computerDice2PointValue;
+var computerDice3PointValue;
+var userCoins;
+var computerCoins;
+
+//these are the functions related to game play
+var playerWin() {};
+var playerLose() {};
+var tieGame() {};
+
+var rollDice() {};
+var nextTurn() {};
+var newRound() {};
+var newPlayer() {};
+var computerPlayFirst() {};
+var userPlayFirst() {};
+var winCoin() {};
+var loseCoin() {};
+
+var calcRollScore() {};
+var compareScore() {};
+var autoLose() {};
+var autoWin() {};
+var validateAnswers() {};
+var setTimeout() {};
+var pauseGame() {};
+var playPrompt() {};
+
+
+//These are the functions related to events
+var diceAudio() {};
+var cheerAudio() {};
+var awAudio() {};
+var backgroundAudio() {};
+var coinAudio() {};
+var rollOverAudio() {};
+var hoverToneAudio() {};
+var popAudio() {};
+var stopAudio() {};
+var laughAudio() {};
+var shake() {};
+var throwIncrease() {};
+var popElement() {};
+var pushInElement() {};
+
+
+
+
